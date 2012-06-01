63f16086a08e5cea546a8e81018d55f341ea6c6c
Rob Boyle <rboyle@gmail.com>
2012-06-01 15:18:23 -0700

    added stub tests and updated the README

--- README.md
+++ README.md
@@ -1,17 +1,116 @@
 Install
 -------
-Install from pypi using pip:
-
-    $ pip install legit
-    
-Or checkout the source and install from there:
+Checkout the source and install from there:
 
     $ git clone http://github.com/LegitInc/legit-python
     $ cd legit-python
     $ python setup.py install
     
-The final command may need to be run with `sudo`.
+The final command may need to be run with `sudo`, depending on your system's configuration.
 
 Usage
 -----
+The library can be used both to submit to data as well as to request data. The interface closely mirrors the REST api, so reference the [REST api docs](http://legitapi.appspot.com/docs/apis/) for more information.
+
+### API Credentials
+The `LegitClient` needs your consumer key and secret to authenticate it's calls:
+
+    from legit import LegitClient
+    
+    key = "XXXXXXXXXXXXXXX"
+    secret = "XXXXXXXXXXXXXXX"
+    client = LegitClient(key, secret)
+
+Alternatively, you can create a `LegitClient` without passing in a key and secret. In this case, it will look for your key and secret in the `LEGIT_KEY` and `LEGIT_SECRET` environment variables. 
+
+We highly suggest keeping your credentials in environment variables. This keeps your from having to keep your key/secret in source control or anywhere other than in the session executing your calls.
+
+### Submitting Data
+First, create a client as shown above.
+
+    from legit import LegitClient
+
+    key = "XXXXXXXXXXXXXXX"
+    secret = "XXXXXXXXXXXXXXX"
+    client = LegitClient(key, secret)
+
+Although we have them in code here, it's really best to store your credentials in environment variables.    
+
+We also need to specify the user we are submitting, using a `LegitUser` object:
+
+    from legit import LegitUser
+    
+    user = LegitUser(user_id=12345, name="Test User", email="user@site.com", 
+                     facebook_id=203935, twitter_id=4935893)
+
+We then submit this user and their activity to Legit:
+
+    response = client.submit(user, 
+                             transaction_count=45, 
+                             review_count=23,
+                             positive_review_percentage=56.3, 
+                             date_joined="2009-04-23",
+                             date_banned="2011-06-03",
+                             reason_banned="Theft of property from other users.")
+
+If all went well, we can print the response and get something along the lines of:
+
+    {
+        u'user_id': u'12345', 
+        u'is_new': False
+    }
+
+### Querying Data
+Create the client as before:
+
+    from legit import LegitClient
+    
+    key = "XXXXXXXXXXXXXXX"
+    secret = "XXXXXXXXXXXXXXX"
+    client = LegitClient(key, secret)
+
+Also create the user as before. Only specify a `user_id` if you wish to query one of your existing users. If you are searching for a user who is not currently in your system, search by the other identifiers you have for them.
+
+    from legit import LegitUser
+
+    user = LegitUser(name="Test User", email="user@site.com", 
+                     facebook_id=203935, twitter_id=4935893)
+
+Decide which report components you would like to request. Then pass the list of
+components along with the `LegitUser` to Legit.
+
+    components = ["blacklist"]
+    
+    response = client.report(components, user)
+
+If all went well, we can print the response. In the case of a blacklist call like the one above, our response might looks something like:
+
+    {
+        "legit_score": 35,    
+        "blacklist": {
+            "blacklisting_count": 1,
+            "blacklistings": [
+                {
+                    "marketplace_type": "Toaster Sharing",
+                    "date": "2010-04-15",
+                    "reason": "Repeated theft of goods from other members."
+                },
+            ],
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- legit/__init__.py
+++ legit/__init__.py
@@ -1,3 +1,7 @@
+__version_info__ = ('0', '1')
+__version__ = '.'.join(__version_info__)
+
+import os
 import oauth2
 import urllib
 try:
@@ -5,58 +9,133 @@ try:
 except ImportError:
     import simplejson as json
 
-BASE_URL = "https://legitapi.appspot.com/"
+BASE_URL = "https://legitapi.appspot.com"
 
-class RequestError(Exception):
+class LegitBadRequestError(Exception):
+    "Exception thrown when a request is bad"
     pass
 
-class LegitClient(object):
-    def __init__(self, consumer_key, consumer_secret):
-        self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret
+class LegitPermissionError(Exception):
+    "Exception thrown when a permission error is encountered"
+    pass
+
+class LegitUser(object):
+    """
+    Container class for all the identity aspects that the Legit API takes into
+    account when specifying a user.
+    """
+    def __init__(self, user_id=None, name=None, address=None, ssn=None, 
+                 phone=None, email=None, facebook_id=None, twitter_id=None, 
+                 linkedin_id=None, drivers_lic=None):
+        self.user_id=user_id
+        self.name=name
+        self.address=address
+        self.ssn=ssn
+        self.phone=phone
+        self.email=email
+        self.facebook_id=facebook_id
+        self.twitter_id=twitter_id
+        self.linkedin_id=linkedin_id
+        self.drivers_lic=drivers_lic
         
-        consumer = oauth2.Consumer(key=consumer_key, secret=consumer_secret)
+    def to_dict(self):
+        rv = {}
+        for prop in ("user_id", "name", "address", "ssn", "phone", "email", 
+                     "facebook_id", "twitter_id", "linkedin_id", "drivers_lic"):
+            attr = getattr(self, prop, None)
+            if attr != None:
+                rv[prop] = attr
+        return rv
+
+class LegitClient(object):
+    """
+    The primary class for interacting with the Legit API. You have the option
+    of creating it with your authentication key/secret. If you do not specify
+    these, the client will attempt to read them out of the environment variables
+    LEGIT_KEY and LEGIT_SECRET respectively. 
+    
+    The LegitClient's has a method for each resource of the Legit API. The 
+    parameters of the client methods are the same as that of the API method.
+    However, the identity elements of a call (user_id, name, email, etc) are 
+    passed in via a LegitUser object. 
+    """
+    def __init__(self, consumer_key=None, consumer_secret=None):
+        self.consumer_key = consumer_key or os.environ.get("LEGIT_KEY")
+        self.consumer_secret = consumer_secret or os.environ.get("LEGIT_SECRET")
+        
+        consumer = oauth2.Consumer(key=self.consumer_key, secret=self.consumer_secret)
         self.client = oauth2.Client(consumer)
     
-    def _make_request(self, data, method, resource, sandbox):
-         api = "sandbox" if sandbox else "api"        
-         url = "%s/%s/%s" % (BASE_URL, api, resource)
+    def make_request(self, data, resource, category, method, sandbox):
+        api = "sandbox" if sandbox else "api"        
+        url = "%s/%s/%s/%s" % (BASE_URL, api, category, resource)
 
-         resp, content = self.client.request(url, method, urllib.urlencode(data))
+        resp, content = self.client.request(url, method, urllib.urlencode(data))
 
-         if resp != 200:
-             raise RequestError(content)
+        if resp.status == 400:
+            raise LegitBadRequestError("%d: %s" % (resp.status,content))
+        elif resp.status == 403:
+            raise LegitPermissionError("%d: %s" % (resp.status,content))
+        elif resp.status != 200:
+            raise Exception("%d: %s" % (resp.status,content))
 
-         return json.loads(content)
+        return json.loads(content)
     
-    def submit(self, parameters, sandbox=False):
+    def submit(self, legit_user, transaction_count, review_count, 
+               positive_review_percentage, date_joined, date_banned=None,
+               reason_banned=None, sandbox=False):
         """
         Invokes the submit method of the Legit API. 
         
         Args:
-            parameters: Dictionary of data that identitifies a user. See
-            the API docs for information on the specific fields.
+            legit_user: A LegitUser object containing all the information needed
+                to identity this user.
+            
+            transaction_count: Number of transaction the user has successfully completed
+            review_count: Number of reviews for the user
+            positive_revew_percentage: Percentage of review for the user whihch
+                are positive
+            date_joined: Date the user joined as a string in the format "YYYY-MM-DD"
+            date_banned (optional): Date the user was banned in the format "YYYY-MM-DD"
+            reason_banned (optional): Reason the user was banned
             
             sandbox: Boolean that dictates whether the request should be made
-            against the sandbox API or the production API.
+                against the sandbox API or the production API.
 
-        Returns: A dictionary of request data.
+        Returns: 
+            A dictionary of response data. See the Legit API docs for the
+            precise structure of the response.
         """
-        return self._make_request(parameters, "submit", "POST", sandbox)
+        data = legit_user.to_dict()
+        data["transaction_count"] = transaction_count
+        data["review_count"] = review_count
+        data["positive_review_percentage"] = positive_review_percentage
+        data["date_joined"] = date_joined
+        if date_banned != None:
+            data["date_banned"] = date_banned
+        if reason_banned != None:
+            data["reason_banned"] = reason_banned
         
+        return self.make_request(data, "user", "submit", "POST", sandbox)    
                
-    def report(self, components, parameters, sandbox=False):
+    def report(self, components, legit_user, sandbox=False):
         """
-        Invokes the report method of the Legit API
+        Invokes the report method of the Legit API.
         
+        Args:
+            components: List of components you wish to include in the report.
+            legit_user: A LegitUser object containing all the data needed to
+                identity this user.
+            
+            sandbox: Boolean that dictates whether the request should be made
+                against the sandbox API or the production API.        
         """
         # Validate the components/identity data
         
-        request_data = {}
-        request_data.update(parameters)
-        request_data["components"] = ",".join(components)
+        data = legit_user.to_dict()
+        data["components"] = ",".join(components)
         
-        return self._make_request(request_data, "report", "GET", sandbox)
+        return self.make_request(data, "report", "query", "GET", sandbox)
         
         
         
